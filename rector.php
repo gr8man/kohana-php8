6a7e59cc9210aa70a1066cdc870d1958fd354eec
Bogdan <matolq@gmail.com>
2026-06-24 22:58:12 +0200

    chore: add static analysis and refactoring configuration for PHPStan and Rector

---
+++ rector.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/application/classes',
+        __DIR__ . '/modules',
+        __DIR__ . '/system/classes',
+    ])
+    ->withSkip([
+        __DIR__ . '/vendor',
+        __DIR__ . '/system/tests',
+        __DIR__ . '/modules/*/tests',
+        __DIR__ . '/application/tests',
+    ])
+    ->withPhpSets(
+        php82: true
+    )
+    ->withPreparedSets(
+        deadCode: true,
+        typeDeclarations: true
+    );
